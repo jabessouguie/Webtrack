64dd4110ff01b60369f34e6e21d7c9095fe6624a
jabessouguie <57636903+jabessouguie@users.noreply.github.com>
2020-01-05 18:51:54 +0100

    Ajout d'un compteur

--- monScript.js
+++ monScript.js
@@ -1,19 +1,24 @@
-var finish = false;
+var victore = false;
+var deplacements = document.getElementById("deplacement");
+var compteur = 0;
+var bouge = true;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
  }
   
 
-async function refill()
+async function mix()
 {
+    bouge = true;
+    compteur = 0;
+    deplacements.textContent = compteur;
     for(i=0;i<100;i++)
     {
         rowNum1 = Math.floor(Math.random() * 4);        
         rowNum2 = Math.floor(Math.random() * 4);
         colNum1 = Math.floor(Math.random() * 4)+1;
         colNum2 = Math.floor(Math.random() * 4)+1;
-        //alert(rowNum1 + ' - ' + rowNum2 + ' - ' +  colNum1 + ' - ' + colNum2  );
         if((rowNum1 != rowNum2)||(colNum1 != colNum2))
         {            
             tempo = document.getElementById("matrix").children[rowNum1].children[colNum1].children[0].innerHTML;
@@ -33,18 +38,20 @@ async function refill()
 
 function moveElement(num)
 {
-    if (finish) return;
+    move = false;
+    if (victoire) return;
     rowNum = Math.floor(num /4);
-    colNum = (num % 4) + 1;        
+    colNum = (num % 4) + 1; 
+    // Il s'agit juste de bouger en vérifiant que la case n'est pas au bout. Surement long et pas optimal mais bon
     if ((rowNum>0) && (document.getElementById("matrix").children[rowNum-1].children[colNum].children[0].innerHTML=="*"))        
     {
         document.getElementById("matrix").children[rowNum-1].children[colNum].children[0].innerHTML = document.getElementById("matrix").children[rowNum].children[colNum].children[0].innerHTML;
         document.getElementById("matrix").children[rowNum].children[colNum].children[0].innerHTML = "*";
         document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("cell");
-        document.getElementById("matrix").children[rowNum-1].children[colNum].children[0].classList.toggle("btn-danger");
-        document.getElementById("matrix").children[rowNum-1].children[colNum].children[0].classList.toggle("btn-info");        
-        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("btn-danger");
-        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("btn-info");  
+        document.getElementById("matrix").children[rowNum-1].children[colNum].children[0].classList.toggle("bouton-danger");
+        document.getElementById("matrix").children[rowNum-1].children[colNum].children[0].classList.toggle("bouton-info");        
+        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("bouton-danger");
+        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("bouton-info");  
         document.getElementById("matrix").children[rowNum-1].children[colNum].children[0].classList.toggle("cell");  
         verifyTri();      
         return;
@@ -54,10 +61,10 @@ function moveElement(num)
         document.getElementById("matrix").children[rowNum+1].children[colNum].children[0].innerHTML = document.getElementById("matrix").children[rowNum].children[colNum].children[0].innerHTML;
         document.getElementById("matrix").children[rowNum].children[colNum].children[0].innerHTML = "*";
         document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("cell");
-        document.getElementById("matrix").children[rowNum+1].children[colNum].children[0].classList.toggle("btn-danger");
-        document.getElementById("matrix").children[rowNum+1].children[colNum].children[0].classList.toggle("btn-info");       
-        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("btn-danger");
-        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("btn-info");
+        document.getElementById("matrix").children[rowNum+1].children[colNum].children[0].classList.toggle("bouton-danger");
+        document.getElementById("matrix").children[rowNum+1].children[colNum].children[0].classList.toggle("bouton-info");       
+        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("bouton-danger");
+        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("boutonn-info");
         document.getElementById("matrix").children[rowNum+1].children[colNum].children[0].classList.toggle("cell");  
         verifyTri();
         return;
@@ -67,10 +74,10 @@ function moveElement(num)
         document.getElementById("matrix").children[rowNum].children[colNum-1].children[0].innerHTML = document.getElementById("matrix").children[rowNum].children[colNum].children[0].innerHTML;
         document.getElementById("matrix").children[rowNum].children[colNum].children[0].innerHTML = "*";
         document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("cell");        
-        document.getElementById("matrix").children[rowNum].children[colNum-1].children[0].classList.toggle("btn-danger");
-        document.getElementById("matrix").children[rowNum].children[colNum-1].children[0].classList.toggle("btn-info");
-        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("btn-danger");
-        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("btn-info");
+        document.getElementById("matrix").children[rowNum].children[colNum-1].children[0].classList.toggle("bouton-danger");
+        document.getElementById("matrix").children[rowNum].children[colNum-1].children[0].classList.toggle("bouton-info");
+        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("bouton-danger");
+        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("bouton-info");
         document.getElementById("matrix").children[rowNum].children[colNum-1].children[0].classList.toggle("cell");          
         verifyTri();
         return;
@@ -80,10 +87,10 @@ function moveElement(num)
         document.getElementById("matrix").children[rowNum].children[colNum+1].children[0].innerHTML = document.getElementById("matrix").children[rowNum].children[colNum].children[0].innerHTML;
         document.getElementById("matrix").children[rowNum].children[colNum].children[0].innerHTML = "*";
         document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("cell");
-        document.getElementById("matrix").children[rowNum].children[colNum+1].children[0].classList.toggle("btn-danger");
-        document.getElementById("matrix").children[rowNum].children[colNum+1].children[0].classList.toggle("btn-info");        
-        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("btn-danger");
-        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("btn-info");
+        document.getElementById("matrix").children[rowNum].children[colNum+1].children[0].classList.toggle("bouton-danger");
+        document.getElementById("matrix").children[rowNum].children[colNum+1].children[0].classList.toggle("bouton-info");        
+        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("boutonn-danger");
+        document.getElementById("matrix").children[rowNum].children[colNum].children[0].classList.toggle("boutonn-info");
         document.getElementById("matrix").children[rowNum].children[colNum+1].children[0].classList.toggle("cell");  
         verifyTri();        
         return;
@@ -105,14 +112,15 @@ function verifyTri()
     return true;
 }
 
-async function printFinish()
+async function affichageFinal()
 {
+    move = false;
     listBt = document.querySelectorAll("[onmouseover]");  
     for(i=0;i<listBt.length;i++)
     {
-        listBt[i].classList.add("btn-success");
-        listBt[i].classList.remove("btn-danger");
-        listBt[i].classList.remove("btn-info");
+        listBt[i].classList.add("boutonn-succes");
+        listBt[i].classList.remove("boutonn-danger");
+        listBt[i].classList.remove("boutonn-info");
         await sleep(150);
     };
 }
